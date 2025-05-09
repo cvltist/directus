261d6a2a76d147aac17c28641e7534b19bb35ab4
cvltist <96958362+cvltist@users.noreply.github.com>
2025-05-09 13:48:27 -0500

    Fix

--- extensions/hooks/image-upload.js
+++ extensions/hooks/image-upload.js
@@ -1,43 +1,18 @@
-export default function registerHook({ action }, { services, getSchema, env }) {
-    const { ItemsService } = services;
-  
-    action('files.upload', async ({ payload }, { schema, accountability }) => {
-      const fileId = payload.id;
-      const filename = payload.filename_disk;
-      const mimeType = payload.type;
-  
-      const bucket = env['STORAGE_FILES_BUCKET'];
-      const endpoint = env['STORAGE_FILES_ENDPOINT'];
-      const objectKey = filename;
-      const url = `${endpoint}/${bucket}/${objectKey}`;
-  
-      const imagesService = new ItemsService('images', { schema, accountability });
-  
-      const imageRecord = await imagesService.createOne({
-        name: payload.title || filename,
-        content_type: mimeType,
-        minio_bucket: bucket,
-        minio_object_key: objectKey,
-        minio_url: url,
-      });
-  
-      const metadata = payload.metadata || {};
-      const restaurantId = metadata.restaurant_id;
-      const menuItemId = metadata.menu_item_id;
-  
-      if (restaurantId) {
-        const restaurantImages = new ItemsService('restaurant_images', { schema, accountability });
-        await restaurantImages.createOne({
-          restaurant_id: restaurantId,
-          image_id: imageRecord.id,
-        });
-      } else if (menuItemId) {
-        const menuItemImages = new ItemsService('menu_item_images', { schema, accountability });
-        await menuItemImages.createOne({
-          menu_item_id: menuItemId,
-          image_id: imageRecord.id,
-        });
-      }
+export default function registerHook({ action }, { services, env }) {
+  const { ItemsService } = services;
+
+  action('files.upload', async ({ payload }, { schema, accountability }) => {
+    console.log('🔥 Hook triggered for file:', payload.filename_download);
+
+    const imagesService = new ItemsService('images', { schema, accountability });
+    const created = await imagesService.createOne({
+      name: payload.title || payload.filename_download,
+      content_type: payload.type,
+      minio_bucket: env.STORAGE_S3_BUCKET,
+      minio_object_key: payload.filename_disk,
+      minio_url: `${env.STORAGE_S3_ENDPOINT}/${env.STORAGE_S3_BUCKET}/${payload.filename_disk}`,
     });
-  }
-  
+
+    console.log('✅ Image record created:', created);
+  });
+}
